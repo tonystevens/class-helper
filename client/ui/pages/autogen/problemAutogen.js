79cc290083cb7fa75fa822c7714176bf7fddd376
WHQ_NT_DOMAIN\TZ029887 <tony.zhang@cerner.com>
2017-06-14 17:12:13 -0500

    temp

--- client/ui/pages/autogen/problemAutogen.js
+++ client/ui/pages/autogen/problemAutogen.js
@@ -2,15 +2,26 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import { findRandomProblemsets } from '../../../../lib/methods.js';
+const problemsets = new ReactiveVar(undefined);
 
 Template.problemAutogen.onCreated(function onTemplateCreated() {
   this.singleCourse = new ReactiveVar({_id: FlowRouter.getParam('_id'), name: FlowRouter.getQueryParam('name')});
   this.problemTemplate = new ReactiveVar(FlowRouter.getQueryParam('problemTemplate'));
+  Meteor.call(
+    'findRandomProblemsets',
+    function(error, result){
+      if(error){
+        console.log(error);
+      } else {
+        problemsets.set(result);
+      }
+    }
+  );
+  setTimeout(() => {}, 500);
 });
 
 Template.problemAutogen.onRendered(function onTemplateRendered() {
-  console.log(this.problemTemplate.get());
+  console.log(problemsets.get());
 });
 
 Template.problemAutogen.helpers({
