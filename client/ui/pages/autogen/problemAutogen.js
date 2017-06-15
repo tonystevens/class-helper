e6996ce851ed8af89a5da813469cb5b0a076c86c
WHQ_NT_DOMAIN\TZ029887 <tony.zhang@cerner.com>
2017-06-15 09:29:02 -0500

    using promise

--- client/ui/pages/autogen/problemAutogen.js
+++ client/ui/pages/autogen/problemAutogen.js
@@ -2,26 +2,25 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-const problemsets = new ReactiveVar(undefined);
+const retrieveProblemsets = (method) => new Promise((resolve, reject) => {
+	Meteor.call(method, (error, result) => {
+			if(error) reject(error);
+      resolve(result);
+		}
+	);
+});
 
 Template.problemAutogen.onCreated(function onTemplateCreated() {
   this.singleCourse = new ReactiveVar({_id: FlowRouter.getParam('_id'), name: FlowRouter.getQueryParam('name')});
   this.problemTemplate = new ReactiveVar(FlowRouter.getQueryParam('problemTemplate'));
-  Meteor.call(
-    'findRandomProblemsets',
-    function(error, result){
-      if(error){
-        console.log(error);
-      } else {
-        problemsets.set(result);
-      }
-    }
-  );
-  setTimeout(() => {}, 500);
+	this.problemsets = new ReactiveVar(undefined);
 });
 
 Template.problemAutogen.onRendered(function onTemplateRendered() {
-  console.log(problemsets.get());
+	retrieveProblemsets('findRandomProblemsets').then((val) => {
+	  this.problemsets.set(val);
+	  console.log(this.problemsets.get());
+  });
 });
 
 Template.problemAutogen.helpers({
