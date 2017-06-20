6681a67215ac9bb624851f1ce01d479fefa684ae
Tony Zhang <tony546@msn.com>
2017-06-20 00:31:54 -0500

    temp: not working

--- client/main.js
+++ client/main.js
@@ -2,5 +2,15 @@ Meteor.startup(function(){
   if(Meteor.isClient){
     var myApp = new Framework7();
     var $$ = Dom7;
+
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+      })
+      .fail(function (error_message) {
+        console.log(error_message);
+      });
+
   }
 });
+
+getUserLanguage = () => "en";
